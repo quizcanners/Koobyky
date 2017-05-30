b238c55ffa52261ee974cb689b8eb14256e80aad
quizcanners <quizcanners@gmail.com>
2017-05-30 14:53:35 +0300

    Added camera script to adjust to screen sizes.

---
+++ Assets/Scripts/CameraScaler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[ExecuteInEditMode]
+public class CameraScaler : MonoBehaviour {
+
+    public float orthogonalWidth = 7;
+    public float orthogonalHeight = 7;
+	// Use this for initialization
+	void Update () {
+        Camera c = Camera.main;
+
+        float proportion = Screen.width / (float)Screen.height;
+        float target = orthogonalWidth / orthogonalHeight;
+
+        c.orthographicSize = Mathf.Lerp(c.orthographicSize, Mathf.Max(orthogonalHeight, orthogonalWidth * (target / proportion)), Time.deltaTime);
+
+	}
+	
+
+}
